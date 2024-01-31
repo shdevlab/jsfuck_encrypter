c6090e7b47a10d4309960753db015597f540ffd3
semi703 <156795575+semi703@users.noreply.github.com>
2024-01-31 11:16:13 +0900

    Update index.js

--- jsfuck_encoder/index.js
+++ jsfuck_encoder/index.js
@@ -1,1 +1,45 @@
-module.exports = null;
+const jsfuck_data = require("./data.js")
+
+function replaceWords(words){
+    return words.split("").map(e=>{
+        if(!isNaN(e) && e != " ") return e
+        return jsfuck_data.words[e] || "String['fromCharCode']("+e.charCodeAt()+")"
+    }).join("+").replace(/(\d(\+\d)?\+\d)/g, function(x){return x.replace("+", "")});
+}
+
+module.exports = function(str){
+    let result = str;
+
+    result = replaceWords(result)
+
+    while(result.includes("'")) result = result.split("'").map((e, i)=>{
+        if(i%2){
+            return replaceWords(e)
+        }else{
+            return e
+        }}).join("");
+
+    for(var i in jsfuck_data.constructors) result = result.replace(new RegExp(i, "g"), jsfuck_data.constructors[i]+"['constructor']")
+    result = result.replace(/Function/g, jsfuck_data.constructors.Function)
+
+    while(result.includes("'")) result = result.split("'").map((e, i)=>{
+        if(i%2){
+            return replaceWords(e)
+        }else{
+            return e
+        }}).join("");
+
+    for(var i in jsfuck_data.defualts) result = result.replace(new RegExp(i, "g"), jsfuck_data.defualts[i])
+
+    result = result.replace(/\d+/g, function(x){
+        return x.split("").map((e,i)=>{
+            if(i == 0){
+                return jsfuck_data.words[e]
+            }else{
+                return "["+jsfuck_data.words[e]+"]"
+            }
+        }).join("+")
+    })
+
+    return result
+}
